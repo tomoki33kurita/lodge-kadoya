12fa9d88a85bed5387bd0487d1cfd2647f824a88
tomoki33kurita <tomoki33kurita@gmail.com>
2022-12-18 10:32:15 +0900

    Implemented calendar page

--- web/pages/availabillity/index.tsx
+++ web/pages/availabillity/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, SimpleGrid } from "@chakra-ui/react";
 import Head from "next/head";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
@@ -42,110 +42,115 @@ const Availabillity = () => {
             <Box>残りわずか</Box>
           </Center>
         </Center>
-        <Center p={4}>
-          <Box mx={1}>
-            <Calendar
-              defaultValue={dayjs("2022/12").toDate()}
-              calendarType={"US"}
-              formatDay={(_, date) => dayjs(date).format("D")}
-              tileClassName={(a) => {
-                const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
-                switch (tileOfDate) {
-                  case "2022-12-31":
-                    return "react-calendar__full";
-                }
-                switch (tileOfDate) {
-                  case "2022-12-29":
-                  case "2022-12-30":
-                    return "react-calendar__few";
-                }
+        <Center px={4}>
+          <SimpleGrid columns={[1, 1, 2]}>
+            <Box m={1}>
+              <Calendar
+                defaultValue={dayjs("2022/12").toDate()}
+                calendarType={"US"}
+                formatDay={(_, date) => dayjs(date).format("D")}
+                tileClassName={(a) => {
+                  const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
+                  switch (tileOfDate) {
+                    case "2022-12-31":
+                      return "react-calendar__full";
+                  }
+                  switch (tileOfDate) {
+                    case "2022-12-29":
+                    case "2022-12-30":
+                      return "react-calendar__few";
+                  }
 
-                const tileOfDay = dayjs(a.date).day();
-                switch (tileOfDay) {
-                  case SATURDAY:
-                    return "react-calendar__saturday";
-                  case SUNDAY:
-                    return "react-calendar__sunday";
-                }
-                return "";
-              }}
-              activeStartDate={dayjs().toDate()}
-            />
-          </Box>
-          <Box mx={1}>
-            <Calendar
-              defaultValue={dayjs("2023/01").toDate()}
-              calendarType={"US"}
-              formatDay={(_, date) => dayjs(date).format("D")}
-              tileClassName={(a) => {
-                const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
-                switch (tileOfDate) {
-                  case "2023-01-01":
-                  case "2023-01-07":
-                  case "2023-01-08":
-                    return "react-calendar__full";
-                  case "2023-01-09":
-                    return "react-calendar__national__holiday";
-                }
-                const tileOfDay = dayjs(a.date).day();
-                switch (tileOfDay) {
-                  case SATURDAY:
-                    return "react-calendar__saturday";
-                  case SUNDAY:
-                    return "react-calendar__sunday";
-                }
-                return "";
-              }}
-            />
-          </Box>
+                  const tileOfDay = dayjs(a.date).day();
+                  switch (tileOfDay) {
+                    case SATURDAY:
+                      return "react-calendar__saturday";
+                    case SUNDAY:
+                      return "react-calendar__sunday";
+                  }
+                  return "";
+                }}
+                activeStartDate={dayjs().toDate()}
+              />
+            </Box>
+
+            <Box m={1}>
+              <Calendar
+                defaultValue={dayjs("2023/01").toDate()}
+                calendarType={"US"}
+                formatDay={(_, date) => dayjs(date).format("D")}
+                tileClassName={(a) => {
+                  const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
+                  switch (tileOfDate) {
+                    case "2023-01-01":
+                    case "2023-01-07":
+                    case "2023-01-08":
+                      return "react-calendar__full";
+                    case "2023-01-09":
+                      return "react-calendar__national__holiday";
+                  }
+                  const tileOfDay = dayjs(a.date).day();
+                  switch (tileOfDay) {
+                    case SATURDAY:
+                      return "react-calendar__saturday";
+                    case SUNDAY:
+                      return "react-calendar__sunday";
+                  }
+                  return "";
+                }}
+              />
+            </Box>
+          </SimpleGrid>
         </Center>
-        <Center p={4}>
-          <Box mx={1}>
-            <Calendar
-              defaultValue={dayjs("2023/02").toDate()}
-              calendarType={"US"}
-              formatDay={(_, date) => dayjs(date).format("D")}
-              tileClassName={(a) => {
-                const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
-                switch (tileOfDate) {
-                  case "2023-02-11":
-                  case "2023-02-23":
-                    return "react-calendar__national__holiday";
-                }
-                const tileOfDay = dayjs(a.date).day();
-                switch (tileOfDay) {
-                  case SATURDAY:
-                    return "react-calendar__saturday";
-                  case SUNDAY:
-                    return "react-calendar__sunday";
-                }
-                return "";
-              }}
-            />
-          </Box>
-          <Box mx={1}>
-            <Calendar
-              defaultValue={dayjs("2023/03").toDate()}
-              calendarType={"US"}
-              formatDay={(_, date) => dayjs(date).format("D")}
-              tileClassName={(a) => {
-                const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
-                switch (tileOfDate) {
-                  case "2023-03-21":
-                    return "react-calendar__national__holiday";
-                }
+        <Center px={4}>
+          <SimpleGrid columns={[1, 1, 2]}>
+            <Box m={1}>
+              <Calendar
+                defaultValue={dayjs("2023/02").toDate()}
+                calendarType={"US"}
+                formatDay={(_, date) => dayjs(date).format("D")}
+                tileClassName={(a) => {
+                  const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
+                  switch (tileOfDate) {
+                    case "2023-02-11":
+                    case "2023-02-23":
+                      return "react-calendar__national__holiday";
+                  }
+                  const tileOfDay = dayjs(a.date).day();
+                  switch (tileOfDay) {
+                    case SATURDAY:
+                      return "react-calendar__saturday";
+                    case SUNDAY:
+                      return "react-calendar__sunday";
+                  }
+                  return "";
+                }}
+              />
+            </Box>
+            <Box m={1}>
+              <Calendar
+                defaultValue={dayjs("2023/03").toDate()}
+                calendarType={"US"}
+                formatDay={(_, date) => dayjs(date).format("D")}
+                tileClassName={(a) => {
+                  const tileOfDate = dayjs(a.date).format("YYYY-MM-DD");
+                  switch (tileOfDate) {
+                    case "2023-03-21":
+                      return "react-calendar__national__holiday";
+                  }
 
-                const tileOfDay = dayjs(a.date).day();
-                switch (tileOfDay) {
-                  case SATURDAY:
-                    return "react-calendar__saturday";
-                  case SUNDAY:
-                    return "react-calendar__sunday";
-                }
-                return "";
-              }}
-            />
-          </Box>
+                  const tileOfDay = dayjs(a.date).day();
+                  switch (tileOfDay) {
+                    case SATURDAY:
+                      return "react-calendar__saturday";
+                    case SUNDAY:
+                      return "react-calendar__sunday";
+                  }
+                  return "";
+                }}
+              />
+            </Box>
+          </SimpleGrid>
         </Center>
       </main>
     </div>
