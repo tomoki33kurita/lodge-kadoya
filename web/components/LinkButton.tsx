2de1f8962cbb072680517547c2ca2719f7c1ee06
tomoki33kurita <tomoki33kurita@gmail.com>
2022-12-19 01:45:51 +0900

    Updated reserve page

--- web/components/LinkButton.tsx
+++ web/components/LinkButton.tsx
@@ -5,13 +5,32 @@ import Link from "next/link";
 type Props = {
   label: string;
   href: string;
+  height?: string;
+  width?: string;
+  border?: string;
+  borderColor?: string;
+  bgColor?: string;
+  color?: string;
 };
 
-export const LinkButton: React.FC<Props> = ({ label, href }) => {
+export const LinkButton: React.FC<Props> = ({
+  label,
+  href,
+  height,
+  width,
+  border,
+  borderColor,
+  bgColor = "teal.500",
+  color = "white",
+}) => {
   return (
     <Link href={href} passHref>
       <A style={{ textDecoration: "none" }}>
-        <Button color={"white"} bgColor={"teal.500"} size={"lg"}>
+        <Button
+          size={"lg"}
+          // @ts-ignore
+          {...{ color, bgColor, height, width, border, borderColor }}
+        >
           {label}
         </Button>
       </A>
