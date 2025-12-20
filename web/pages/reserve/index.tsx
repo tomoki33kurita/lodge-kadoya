fa48c1a8b4bcee5c96ba6ff7b138e1cc70980ece
tomoki33kurita <tomoki33kurita@gmail.com>
2025-12-20 12:48:43 +0900

    feat: カレンダーを更新

--- web/pages/reserve/index.tsx
+++ web/pages/reserve/index.tsx
@@ -233,7 +233,6 @@ const Reserve: React.FC = () => {
                         case "2026/02/19":
                           return "few" // 平日
                         case "2026/01/10":
-                        case "2026/01/31":
                           return "few__saturday" // 土曜日
                         case "2026/01/11":
                         case "2023/02/22":
@@ -247,11 +246,14 @@ const Reserve: React.FC = () => {
                         case "2025/12/31":
                           return "full"
                         case "2025/12/27":
+                        case "2026/01/03":
                         case "2026/01/17":
                         case "2026/01/24":
+                        case "2026/01/31":
                         case "2026/02/07":
                         case "2026/02/14":
                         case "2026/02/21":
+                        case "2026/02/28":
                         case "2026/03/07":
                         case "2026/03/14":
                         case "2026/03/21":
